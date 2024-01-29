9fdb396bc0f15fe910ae1d3cb427f5cd94f4d647
Hemant <me.hemant02@gmail.com>
2024-01-29 14:07:55 +0530

    Hemant Pachaury

---
+++ src/data/constants.js
@@ -0,0 +1,215 @@
+export const Bio = {
+    name: "Hemant Pachaury",
+    roles: [
+      " Frontend Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/HemantPachaury",
+    resume:
+      "https://www.google.com/",
+    linkedin: "https://www.linkedin.com/in/hemantpachaury8755855116/",
+    twitter: "https://twitter.com/HemantPachaury1",
+    insta: "https://www.instagram.com/hemant_ravi_pachaury/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+       
+       
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+       
+       
+      ],
+    },
+
+
+
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Java",
+          image:
+          "https://www.finoit.com/wp-content/uploads/2022/11/future-of-java-programming-language.jpg"
+        },
+
+        {
+          name: "Cloud Computing",
+          image:
+          "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3BmLXMxMDYtcG0tNjkwNS5qcGc.jpg"
+        },
+
+        {
+          name: "Networking",
+          image:
+          "https://images.shiksha.com/mediadata/shikshaOnline/mailers/2021/naukri-learning/oct/27oct-v2/What-is-Networking.jpg"
+        },
+        
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+       
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+       
+       
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOMZsUfa14ECGzHepQUb4i3hkVeLPRXZCTSu1_gN3u4w&s",
+      role: "Frontend Engineer Intern",
+      company: "Cyberx Infosystem Pvt. Ltd.",
+      date: "March 2023 - Sept.2023",
+      desc: "Working on the frontend of the web application using ReactJS,Javascript.",
+      skills: [
+        "ReactJS",
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "JavaScript",
+      ],
+         },
+   
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+      role: "Open Source & Hackathon Contributor ",
+      company: "Optim Hire Challege",
+      desc: "Contributing to different open-source projects and learn from industry experts",
+          },
+
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+      role: "Open Source & Hackathon Contributor ",
+      company: "Collage Dunia Hiring Challege",
+      desc: "Contributing to different open-source projects and learn from industry experts",
+        }
+    
+   
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/en/6/64/Galgotias_University_logo_seal.jpg",
+      school: "Galgotias University Greater Noida U.P.",
+      date: "Aug 2022 - June 2022",
+      grade: "76%",
+      desc: "I am  graduate with my  Bachelor's degree in Computer Science and Application at Galgotias University , Greater Noida. I have completed my all 6 semesters and have a CGPA of 7.64. I have taken courses in Java, Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Computer Application - BCA, Computer Application  & Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://png.pngtree.com/png-clipart/20211009/original/pngtree-school-logo-png-image_6846798.png",
+      school: "Khair Inter Collage Aligarh U.P.",
+      date: "July 2018 - July 2020",
+      grade: "65%",
+      desc: "I completed my class 12 high school education at Khair Inter Collage Khair, Aligarh , where I studied Science with Maths Physics & Chemistry.",
+      degree: "(XII), Science ",
+    },
+    {
+      id: 2,
+      img: "https://img.freepik.com/free-vector/gradient-school-logo-design-template_23-2149664341.jpg",
+      school: "R. V. Int. Collage Khair Aligarh U.P.",
+      date: "July 2016 - July 2018",
+      grade: "78%",
+      desc: "I completed my class 10 education at R V Int. Collage Khair Aligarh U.P.  where I studied Science .",
+      degree: "(X), Science ",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "SidCup Family Golf",
+      description:
+        "SidCup Family Golf is a web application designed to see the golf and enhance productivity in the workplace. It provides a user-friendly interface for every visitor to keep updtae daily work activities  \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+      image:
+        "https://golf-pass.brightspotcdn.com/dims4/default/46f2710/2147483647/strip/true/crop/812x524+234+0/resize/930x600!/format/webp/quality/90/?url=https%3A%2F%2Fgolf-pass-brightspot.s3.amazonaws.com%2F1d%2Fe5%2F28d52233df6bc681ac4f9dbdf3ce%2F88236.jpg",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "Bootstrap",
+        "Javascript",
+      ],
+      category: "web app",
+      member: [
+        {
+          name: "Hemant Pachaury",
+          img: "http://localhost:3000/static/media/HeroImage1.c1ed806dd0007776366d.jpeg",
+          linkedin: "https://www.linkedin.com/in/hemantpachaury8755855116/",
+          github: "https://github.com/HemantPachaury",
+        },
+      ],
+    },
+    {
+      id: 0,
+      title: "Suresh Security System",
+      description:
+        "Developed a  web application that allows users to search for, play, and pause their favorite security on demand . Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+      image:
+        "https://sureshsecurityservices.com/wp-content/uploads/2022/09/logoooooo-1.jpg",
+      category: "web app",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "Bootstrap",
+        "Javascript",
+      ],
+      member: [
+        {
+          name: "Hemant Pachaury",
+          img: "http://localhost:3000/static/media/HeroImage1.c1ed806dd0007776366d.jpeg",
+          linkedin: "https://www.linkedin.com/in/hemantpachaury8755855116/",
+          github: "https://github.com/HemantPachaury",
+        },
+      ],
+    },
+   
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
